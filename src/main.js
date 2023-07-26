b2c918d98464f8d7b4aeb488cc158053f79e6de8
Majd Developer <majd.developer9@gmail.com>
2023-07-26 23:05:17 +0300

    adding dist

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '@/style/scss/main.scss'
-import '@fortawesome/fontawesome-free/css/all.css'
-createApp(App).use(store).use(router).mount('#app')
