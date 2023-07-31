bb7fd4194181a1c81abe4c7e66a881ea2c68245d
Majd Developer <majd.developer9@gmail.com>
2023-07-31 15:34:47 +0300

    Update code

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,37 +1,23 @@
 <template>
-  <div class="container pt-4 position-relative" @click="logTargetElement">
+  <div class="container pt-4 position-relative">
     <form action="" class="row col-10 col-lg-6 m-auto">
         <div class="bg-success input-group p-0 mb-0 m-auto rounded-circle">
-        <input type="text" class="form-control" placeholder="Enter Movies" aria-label="Username"
+        <input type="text" class="form-control input-search" placeholder="Enter Movies" aria-label="Username"
           aria-describedby="basic-addon1" v-model="movieTarget">
-          <span class="input-group-text p-2 fs-4 cur-pointer btn-search" id="basic-addon1" @click="filteredData()">
-            <i class="fa-solid fa-magnifying-glass p-1"></i>
-          </span>
         </div>
       </form>
-        <!-- <ul class="list-group list-search col-10 col-lg-6 m-auto mt-1 position-absolute" v-if="filteredData.length && movieTarget">
-          <li class="list-group-item list-group-item-action cur-pointer d-flex justify-content-between align-items-center" v-for="movie, index in filteredData" :key="index" @click="showMoviesInPage(movie.id)">
-            <p class="w-auto">{{ movie.title }}</p>
-            <img :src="getPosterMovieUrl(movie.path)" class="img-search p-0" :alt="movie.title" loading="lazy">
-          </li>
-        </ul>
-        <ul class="list-group list-search col-10 col-lg-6 m-auto mt-1 position-absolute" v-else-if="!filteredData.length">
-          <li class="list-group-item p-3">not found movie</li>
-        </ul> -->
-    <div class="movies mt-5 row g-4 justify-content-center justify-content-sm-center">
-      <div class="col-10 col-sm-6 col-md-4 col-lg-3" v-for="movie, index in filteredData" v-show="(startMovie <= index && index < endMovie && !movieTarget) || movieTarget" :key="index">
+    <div class="movies mt-5 row g-4 justify-content-center justify-content-sm-center" v-if="filteredData.length">
+      <div class="col-10 col-sm-6 col-md-4 col-lg-3" v-for="movie, index in filteredData" :key="index">
         <div
         class="movie p-0 m-1 card row justify-content-between bg-dark rounded shadow-sm text-start text-decoration-none">
         <img :src="getPosterMovieUrl(movie.poster_path)" class="card-img-top p-0" :alt="movie.original_title"
           loading="lazy">
         <div class="dec pe-0 py-4">
-          <div class="row pe-0 align-items-center">
-            <h5 class="col-9 text-white text-break m-0">{{ movie.title }}</h5>
-            <div class="col-3 ps-0">
+            <h5 class="text-white text-break m-0">{{ movie.title }}</h5>
+            <div class="py-1">
               <i class="fa-solid fa-star text-warning"></i>
               <small class="fs-6 text-white">{{ movie.vote_average }}</small>
             </div>
-          </div>
           <div class="date pt-1 text-secondary text-uppercase fw-light">
             <i class="fa-regular fa-clock"></i>
             {{ movie.release_date }} ({{ movie.original_language }})
@@ -40,12 +26,15 @@
       </div>
       </div>
     </div>
-    <ul class="btn-group btn-list list-unstyled my-5" role="group">
+    <div class="text-center fs-3 text-white py-5" v-else>
+      Not found Movie
+    </div>
+    <ul class="btn-group btn-list list-unstyled my-5 mb-2" role="group">
       <li class="w-0 rounded-0" v-for="btn,index in Math.ceil(numbersOfMovies / numbersOfMoviesInPage)" :key="index">
         <button type="button" class="btn btn-warning border rounded-0" @click="showMoviesInPage(((index) * numbersOfMoviesInPage), (((index) * numbersOfMoviesInPage) + numbersOfMoviesInPage))">{{index + 1}}</button>
       </li>
     </ul>
-    <div class="statistics row mb-4 flex-wrap">
+    <div class="statistics row mb-4 flex-wrap" v-if="filteredData.length">
       <div class="col-8 col-sm-6 m-auto col-md-4 col-lg-3 my-3">
         <div class="bg-light shadow rounded p-3">
           <p class="fs-4">Titles over 7</p>
@@ -67,7 +56,7 @@
       <div class="col-8 col-sm-6 m-auto col-md-4 col-lg-3 my-3">
         <div class="bg-light shadow rounded p-3">
           <p class="fs-4">Action titles</p>
-        <p class="fw-bold fs-1">{{ movieActive }}</p>
+        <p class="fw-bold fs-1">{{ movieAction }}</p>
         </div>
       </div>
     </div>
@@ -86,10 +75,8 @@ export default {
       titlesOver7: 0.0,
       titlesOver5: 0.0,
       voteCount: 0,
-      movieActive: 0,
-      movieTarget: '',
-      countMovieTarget: 0,
-      arrayTarget: []
+      movieAction: 0,
+      movieTarget: ''
     }
   },
   methods: {
@@ -107,18 +94,16 @@ export default {
       }
     },
     // show or hint ul list in search
-    logTargetElement (event) {
-      if (!event.target.classList.contains('list-search')) {
-        this.movieTarget = ''
-      }
-    },
+    // logTargetElement (event) {
+    //   if (!event.target.classList.contains('list-search')) {
+    //     this.movieTarget = ''
+    //   }
+    // },
     // show movies according page number
     showMoviesInPage (startMovieCount, endMovieCount = startMovieCount + 1) {
-      // Set startMovie and endMovie to show movies within the specified range
       this.startMovie = startMovieCount
       this.endMovie = endMovieCount
-      // Reset the search input
-      // this.movieTarget = ''
+      this.movieTarget = ''
       // Scroll to the top
       window.scrollTo({
         top: 0,
@@ -135,29 +120,24 @@ export default {
       this.titlesOver5 = 0
       this.titlesOver7 = 0
       this.voteCount = 0
-      this.movieActive = 0
+      this.movieAction = 0
       if (this.movieTarget.length === 0) {
         this.fetchData.forEach((movie, index) => {
           if (this.startMovie <= index && index < this.endMovie) {
             if (movie.vote_average >= 7) {
               this.titlesOver7++
             }
-          }
-        })
-        this.fetchData.forEach((movie, index) => {
-          if (this.startMovie <= index && index < this.endMovie) {
             if (movie.vote_average > 5) {
               this.titlesOver5++
             }
-          }
-        })
-        this.fetchData.forEach((movie, index) => {
-          if (this.startMovie <= index && index < this.endMovie) {
             this.voteCount += movie.vote_count
+            if (movie.genre_ids.includes(28)) {
+              this.movieAction++
+            }
           }
         })
       } else {
-        this.filteredData.forEach((movie, index) => {
+        this.filteredData.forEach((movie) => {
           if (movie.vote_average >= 7) {
             this.titlesOver7++
           }
@@ -165,6 +145,9 @@ export default {
             this.titlesOver5++
           }
           this.voteCount += movie.vote_count
+          if (movie.genre_ids.includes(28)) {
+            this.movieAction++
+          }
         })
       }
       const voteCountAsString = String(this.voteCount)
@@ -183,21 +166,21 @@ export default {
       } else if (voteCountAsString.length === 9) {
         this.voteCount = `${voteCountAsString.slice(0, 3)}M`
       }
-      this.fetchData.forEach((movie, index) => {
-        if (this.startMovie <= index && index < this.endMovie) {
-          this.movieActive++
-        }
-      })
-      this.movieActive = (this.movieActive * 100) / 40 + '%'
+      this.movieAction = Math.floor((this.movieAction * 100) / this.filteredData.length) + '%'
     }
   },
   computed: {
     filteredData () {
-      this.statistics()
-      const filtering = new RegExp(this.movieTarget, 'i')
-      return this.arrayTarget.filter((el) => {
-        return el.title.match(filtering)
-      })
+      if (this.movieTarget.length > 0) {
+        const filtering = new RegExp(this.movieTarget, 'i')
+        return this.fetchData.filter((mov) => {
+          return mov.title.match(filtering)
+        })
+      } else {
+        return this.fetchData.filter((movie, index) => {
+          return this.startMovie <= index && index < this.endMovie
+        })
+      }
     }
   },
   watch: {
@@ -205,7 +188,7 @@ export default {
       handler () {
         this.statistics()
       },
-      immediate: true // Run the handler immediately after creating the watcher
+      immediate: true
     }
   },
   async created () {
@@ -215,16 +198,8 @@ export default {
     this.numbersOfMovies = this.fetchData.length
     this.endMovie = this.numbersOfMoviesInPage
     this.fetchData.forEach((el, index) => {
-      const objMovie = {
-        id: index,
-        title: el.original_title,
-        release_date: el.release_date,
-        original_language: el.original_language,
-        vote_average: el.vote_average,
-        vote_count: el.vote_count,
-        poster_path: this.getPosterMovieUrl(el.poster_path)
-      }
-      this.arrayTarget.push(objMovie)
+      const id = 'id'
+      el[id] = index
     })
   },
   name: 'MainComponent'
